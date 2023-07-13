f68ce9e465855ad4bb9b67e7bf8348cb9932ce4e
Susan Fung <fung_susan@hotmail.com>
2023-07-13 08:59:29 -0600

    Created test method

---
+++ src/main/java/org/rockpaperscissor/Main.java
@@ -0,0 +1,7 @@
+package org.rockpaperscissor;
+
+public class Main {
+    public static String main() {
+        return "Hello world!";
+    }
+}
---
+++ src/test/java/org/rockpaperscissor/MainTest.java
@@ -0,0 +1,13 @@
+package org.rockpaperscissor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertEquals(Main.main(), "Hello world!");
+    }
+}
